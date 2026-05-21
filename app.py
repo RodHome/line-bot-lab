1d50b8b626ba98d1bb1542d38aa7e93a6218c04e
RodHome <108208955+RodHome@users.noreply.github.com>
2026-05-21 09:36:42 +0800

    加入新模型 3.5flash

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def call_gemini_json(prompt, system_instruction=None, schema=None):
     if not keys: return None, "No API Key"
     random.shuffle(keys)
     
-    target_models = ["gemini-2.5-flash","gemini-2.5-flash-lite", "gemma-4-31b-it"]
+    target_models = ["gemini-3.5-flash","gemini-2.5-flash","gemini-2.5-flash-lite"]
     final_prompt = prompt 
     
     for model in target_models:
